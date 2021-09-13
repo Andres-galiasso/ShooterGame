d7e902c5436746bbe33abcadaa5fd9c414ab53f9
Andres-galiasso <andres@galiasso.net>
2021-09-13 16:41:53 -0500

    Player Object Created
    Setup player object
    Started PlayerController script

---
+++ Galiasso-ShooterGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+/*
+ * Created by: Andres Galiasso
+ * Date Created: 09/13/2021
+ *
+ * Last Edited by: Andres Galiasso
+ * Last Updated: 09/13/2021
+ * 
+ * Description: Controls the player ship
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
